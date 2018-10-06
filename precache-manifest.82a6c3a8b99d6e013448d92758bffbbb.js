71f86cac2139768ba06a0313344faae7a8884755
Kevin Nguyen <kvn219@nyu.edu>
2018-10-06 12:31:20 -0400

    deploy

---
+++ precache-manifest.82a6c3a8b99d6e013448d92758bffbbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/menu/robots.txt"
+  },
+  {
+    "revision": "691c9c8628293b9e20b0",
+    "url": "/menu/js/chunk-vendors.cd9667c3.js"
+  },
+  {
+    "revision": "ddfdbb4d6f9f550f1716",
+    "url": "/menu/js/app.235cb80e.js"
+  },
+  {
+    "revision": "36131e19d3dc2b56516e",
+    "url": "/menu/js/about.bf6c88b8.js"
+  },
+  {
+    "revision": "bb945dd276009f58dffe216bff660652",
+    "url": "/menu/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/menu/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ddfdbb4d6f9f550f1716",
+    "url": "/menu/css/app.22e3537a.css"
+  }
+];
